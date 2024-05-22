53f409868094b64c4351e5cf4b508aa3a362ebe7
Joel <115259065+fulcrum44@users.noreply.github.com>
2024-05-22 14:56:27 +0200

    Add files via upload

---
+++ inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO heroes (nombre, vida, ataque_p, ataque_m, defensa_p, defensa_m, velocidad) VALUES
+('El perfeccionista', , , , , , ),
+('El narcisista', , , , , , ),
+('El pudre colegios', , , , , , ),
+('El exterminador tiktoker', , , , , , );
+
+INSERT INTO villanos (nombre, vida, ataque_p, ataque_m, defensa_p, defensa_m, velocidad) VALUES
+('', , , , , , ),
+('', , , , , , ),
+('', , , , , , ),
+('', , , , , , );
