61cbf6dc6e5094f406c004ec26caa3749b3f7039
Vargas Jose Miguel <josemiguel_pilas@hotmail.com>
2024-05-22 14:38:01 +0200

    Procedure Prefight

---
+++ procedure_prefight.sql
@@ -0,0 +1,22 @@
+-- Procedure Prefight
+DROP PROCEDURE IF EXISTS prefight;
+delimiter //
+CREATE PROCEDURE prefight (
+	in h_nombre varchar(20),
+    in v_nombre varchar(20)
+)
+BEGIN
+	DECLARE h_id, h_vida INT;
+    DECLARE v_id, v_vida INT;
+    
+    -- Obtenemos las variables que necesitamos para el combate
+    SELECT id INTO h_id FROM heroe WHERE nombre = h_nombre;
+    SELECT vida INTO h_vida FROM heroe WHERE nombre = h_nombre;
+    
+    SELECT id INTO v_id FROM villano WHERE nombre = v_nombre;
+    SELECT vida INTO v_vida FROM heroe WHERE nombre = h_nombre;
+    
+    INSERT INTO combate (heroe_id, heroe_vida, villano_id, villano_vida) VALUES
+    (h_id, h_vida, v_id, v_vida);
+END
+//
