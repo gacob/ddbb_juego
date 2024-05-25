ccc099db659362f8ecae623e74e7ee2dfc5884bb
Fulcrum <joelchicano70@gmail.com>
2024-05-25 13:04:34 +0200

    Inserts update

--- inserts.sql
+++ inserts.sql
@@ -1,11 +1,24 @@
-INSERT INTO heroes (nombre, vida, ataque_p, ataque_m, defensa_p, defensa_m, velocidad) VALUES
-('El perfeccionista', , , , , , ),
-('El narcisista', , , , , , ),
-('El pudre colegios', , , , , , ),
-('El exterminador tiktoker', , , , , , );
+INSERT INTO heroe (nombre, vida, ataque_p, ataque_m, defensa_p, defensa_m, velocidad) VALUES
+('Aetheron', 100, 20, 15, 10, 5, 30),
+('Blazara', 100, 18, 20, 12, 8, 25),
+('Calyrex', 100, 22, 10, 15, 10, 20),
+('Draxor', 100, 25, 5, 18, 12, 28),
+('Elara', 100, 30, 18, 20, 15, 22),
+('Falkor', 100, 17, 22, 13, 7, 27),
+('Glyndor', 100, 19, 16, 11, 9, 29),
+('Helia', 100, 23, 12, 14, 11, 21),
+('Icarus', 100, 27, 14, 16, 13, 24),
+('Jorvax', 100, 21, 19, 17, 14, 26);
 
-INSERT INTO villanos (nombre, vida, ataque_p, ataque_m, defensa_p, defensa_m, velocidad) VALUES
-('', , , , , , ),
-('', , , , , , ),
-('', , , , , , ),
-('', , , , , , );
+
+INSERT INTO villano (nombre, vida, ataque_p, ataque_m, defensa_p, defensa_m, velocidad) VALUES
+('Zarvox', 100, 21, 18, 12, 6, 28),
+('Kragor', 100, 20, 22, 14, 10, 26),
+('Mordrax', 100, 24, 16, 15, 11, 23),
+('Nexara', 100, 26, 10, 18, 14, 29),
+('Ophira', 100, 28, 15, 20, 16, 25),
+('Pyrax', 100, 22, 20, 17, 8, 27),
+('Qorath', 100, 25, 17, 11, 12, 30),
+('Ravena', 100, 19, 21, 13, 9, 24),
+('Sylvar', 100, 27, 13, 16, 15, 22),
+('Thalor', 100, 23, 19, 18, 13, 26);
